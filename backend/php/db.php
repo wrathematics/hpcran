119a4361dc418744dcb74508636b01e792483e72
wrathematics <wrathematics@gmail.com>
2020-02-22 14:09:25 -0500

    use prepared statement in db call

--- backend/php/db.php
+++ backend/php/db.php
@@ -2,8 +2,11 @@
 
 function authorized_users_lookup($db, $acct)
 {
-  $query = "SELECT COUNT(*) AS count FROM users WHERE account=";
-  $res = $db->query($query . "'" . $acct . "'");
+  $query = "SELECT COUNT(*) AS count FROM users WHERE account=:acct";
+  $prepared = $db->prepare($query);
+  $prepared->bindValue(':acct', $acct, SQLITE3_TEXT);
+  $res = $prepared->execute();
+  
   $ct = $res->fetchArray()['count'];
   return ($ct > 0);
 }
